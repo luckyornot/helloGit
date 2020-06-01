5a1c0d896853091638be3fc266c31b835e3a902d
whw_test <whw_test@qq.com>
2020-06-01 15:05:32 +0800

    My First Commit use Git in IDEA

---
+++ HelloGit/src/main/java/com/whw/git/HelloGit.java
@@ -0,0 +1,14 @@
+package com.whw.git;
+
+/**
+ * @ Description：
+ * @ Author wang
+ * @ Create 2020-06-01-14:42
+ */
+public class HelloGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+
+}
